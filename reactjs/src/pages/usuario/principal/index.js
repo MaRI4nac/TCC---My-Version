b4d17cb8dd0aace6c509a317f68528fb9ce8bca4
LevyCS <levyfronto@gmail.com>
2021-09-20 15:42:37 -0300

    ok

---
+++ reactjs/src/pages/usuario/principal/index.js
@@ -0,0 +1,10 @@
+
+
+
+export default function Principal() {
+    return (
+        <div>
+            <h1> oi :) </h1>
+        </div>
+    )
+}
--- reactjs/src/routes.js
+++ reactjs/src/routes.js
@@ -1,11 +1,11 @@
 import { BrowserRouter, Switch, Route } from "react-router-dom";
-
+import Principal from './pages/usuario/principal/index.js'
 
 export default function Routes() {
     return (
         <BrowserRouter>
             <Switch> 
-                <Route path="/" exact={true} component={}> </Route>
+                <Route path="/" exact={true} component={Principal} />
             </Switch>
         </BrowserRouter>
     )
